501a7bcbc0c8d9de95dcbae6d8dc80bb206fe979
sakusaku000 <harry.spncr@gmail.com>
2026-05-13 21:28:18 +0100

    removed debug background

---
+++ src/components/handlers/RetroMessageDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- debug component -->
+    
+</template>
+
+<script lang="ts" setup>
+    import { useMessageDisplayStore } from '@/stores/MessageDisplayStore';
+    import DebugMessages from "@/data/DebugMessages.json";
+
+    // -- import message display store
+    const messageDisplayStore = useMessageDisplayStore();
+    // -- import debug message data
+    const debugMessage = DebugMessages[10];
+</script>
